86501d8d79207805715db48eb53a4073896bdf60
wangzeyao <blithetyrell@gmail.com>
2019-01-31 15:05:41 +0100

    数据结构，自定义模块，错误处理
    增加数据结构，导入自定义模块，错误处理的学习记录

---
+++ files/pck/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 14:41:50 2019
+
+@author: WANGZEYAO
+"""
+
---
+++ files/pck/printData.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 14:40:00 2019
+
+@author: WANGZEYAO
+"""
+
+def print_data(data):
+    print(data)
