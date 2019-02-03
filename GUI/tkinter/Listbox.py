9a539e53094034dd4e58e726f47c18ec2b4cb10a
wangzeyao <blithetyrell@gmail.com>
2019-02-03 12:01:21 +0100

    Listbox,Radiobutton
    新加Listbox,Radiobutton的学习

---
+++ GUI/tkinter/Listbox.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  3 11:24:32 2019
+
+@author: WANGZEYAO
+"""
+import tkinter as tk
+
+window = tk.Tk()
+window.title('Listbox')
+window.geometry('300x300')
+
+### 创建一个lable用于显示
+var1 = tk.StringVar()
+lable = tk.Label(window,
+                 bg = 'yellow',
+                 width = 9,
+                 textvariable = var1)
+lable.pack()
+
+
+### 定义listbox
+
+var2 = tk.StringVar()
+var2.set((11,22,33,44)) # 给定初始值
+
+lb = tk.Listbox(window,
+                listvariable=var2)
+lb.pack()
+###在列表中插入元素
+list_items = [1,2,3,4]
+for item in list_items:
+    lb.insert('end',item)
+lb.insert(1,'first')
+lb.insert(2,'second')
+
+### 创建一个按钮的点击事件
+def print_selection():
+    value = lb.get(lb.curselection()) # 获得光标当前选定的东西curselection
+    var1.set(value)
+    
+### 创建按钮
+btn = tk.Button(window,
+                text = 'print selection',
+                command = print_selection)
+btn.pack()  
+    
+window.mainloop()
---
+++ GUI/tkinter/Radiobutton.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  3 11:48:29 2019
+
+@author: WANGZEYAO
+"""
+
+import tkinter as tk
+
+### window setup
+window = tk.Tk()
+window.title('Radiobutton')
+window.geometry('300x300')
+
+### 创建显示lable
+var0 = tk.StringVar()
+var1 = tk.StringVar()
+lable = tk.Label(window,
+                 bg = 'yellow',
+                 width = 29,
+                 textvariable = var0)
+lable.pack()
+
+### 创建一个按钮的点击事件
+def print_selection():
+    var0.set('you have selected '+var1.get())
+
+###创建 radiaobutton
+rb1 = tk.Radiobutton(window,
+                    text = 'option A',
+                    variable = var1,value = 'A',#当选中时，将var1赋值成A
+                    command = print_selection)
+rb1.pack()
+
+rb2 = tk.Radiobutton(window,
+                    text = 'option B',
+                    variable = var1,value = 'B',#当选中时，将var1赋值成B
+                    command = print_selection)
+rb2.pack()
+
+
+rb3 = tk.Radiobutton(window,
+                    text = 'option C',
+                    variable = var1,value = 'C',#当选中时，将var1赋值成C
+                    command = print_selection)
+rb3.pack()
+
+
+
+window.mainloop()
